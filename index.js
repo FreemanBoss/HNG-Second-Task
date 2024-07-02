c3766d6669682b091aa7ae92d1d49d8efa156fc9
Programmer-Shams <ssdeen313@gmial.com>
2024-07-02 19:12:49 +0100

    HNG Backend server Deployment

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = 4000;
+
+app.get('/api/hello', async (req, res) => {
+  const visitorName = req.query.visitor_name;
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  console.log("Received request from IP:", clientIp);
+  console.log("Visitor name:", visitorName);
+
+  try {
+    const locationResponse = await axios.get("http://ip-api.com/json/");
+    const locationData = locationResponse.data;
+
+    // console.log("Location data received:", locationData); 
+
+    const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.OPENWEATHERMAP_API_KEY}&q=${locationData.city}&aqi=yes`);
+    
+    const weatherData = weatherResponse.data;
+
+    console.log("Weather data received:", weatherData);
+
+    res.json({
+      client_ip: clientIp,
+      location: locationData.city,
+      greeting: `Hello, ${visitorName}!, the temperature is ${weatherData.current.temp_f} degrees Celsius in ${locationData.city}`
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
