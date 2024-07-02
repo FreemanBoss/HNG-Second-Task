caa33e1473db9b39985043a1166c4144df2637be
Programmer-Shams <ssdeen313@gmial.com>
2024-07-02 20:07:25 +0100

    done

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 const port = 4000;
 
-app.get('/api/hello', async (req, res) => {
-  const visitorName = req.query.visitor_name;
-  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-  console.log("Received request from IP:", clientIp);
-  console.log("Visitor name:", visitorName);
-
+app.get("/api/hello", async (req, res) => {
   try {
     const locationResponse = await axios.get("http://ip-api.com/json/");
     const locationData = locationResponse.data;
+    const clientIp = locationData.query;
 
-    // console.log("Location data received:", locationData); 
+    console.log("Received request from IP:", clientIp);
 
-    const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.OPENWEATHERMAP_API_KEY}&q=${locationData.city}&aqi=yes`);
-    
+    const weatherResponse = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.OPENWEATHERMAP_API_KEY}&q=${locationData.city}&aqi=yes`
+    );
+
     const weatherData = weatherResponse.data;
 
-    console.log("Weather data received:", weatherData);
+    const visitorName = req.query.visitor_name;
 
     res.json({
       client_ip: clientIp,
       location: locationData.city,
-      greeting: `Hello, ${visitorName}!, the temperature is ${weatherData.current.temp_f} degrees Celsius in ${locationData.city}`
+      greeting: `Hello, ${visitorName}!, the temperature is ${weatherData.current.temp_f} degrees Celsius in ${locationData.city}`,
     });
   } catch (error) {
     console.error("Error fetching data:", error);
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
